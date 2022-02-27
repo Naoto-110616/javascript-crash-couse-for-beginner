c04ce987be4bf9fafa0eac0f2471733dda0084f8
Naoto-110616 <siodeitadaku@gmail.com>
2022-02-27 23:23:36 +0900

    how to use JSON stringify

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const person = {
-	firstName: "John",
-	lastName: "Doe",
-	age: 30,
-	hobbies: ["music", "movies", "sports"],
-	address: {
-		street: "50 main st",
-		city: "Boston",
-		state: "MA",
+const todos = [
+	{
+		id: 1,
+		text: "Take out trash",
+		isCompleted: true,
 	},
-};
+	{
+		id: 2,
+		text: "Meeting with boss",
+		isCompleted: true,
+	},
+	{
+		id: 3,
+		text: "Dentist appt",
+		isCompleted: false,
+	},
+];
 
-const {
-	firstName,
-	lastName,
-	address: { city },
-} = person;
+const todoJSON = JSON.stringify(todos);
 
-console.log(city);
+console.log(todoJSON);
