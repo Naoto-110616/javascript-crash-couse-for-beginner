abe5a035ae807555485063b5396284edf07a1f68
ii-158 <okawa@ideaimage.co.jp>
2022-02-28 13:55:23 +0900

    how to use dom class edit

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-const ul = document.querySelector(".items")
-
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = "Hello"
-ul.children[1].innerText = "Brad"
-ul.lastElementChild.innerHTML = "<h1>Hello</h1>"
-
 const btn = document.querySelector(".btn")
-btn.style.background = "red"
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault()
+	document.querySelector("#my-form").style.background = "#ccc"
+	document.querySelector("body").classList.add("bg-dark")
+})
