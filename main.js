cad5d425cd09330f95c1501d22390d829807a7a6
ii-158 <okawa@ideaimage.co.jp>
2022-02-28 13:26:09 +0900

    how to use function protype

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ function Person(firstName, lastName, dob) {
 	this.lastName = lastName;
 	this.dob = new Date(dob);
 }
+Person.prototype.getBirthYear = function () {
+	return this.dob.getFullYear()
+}
+Person.prototype.getFullName = function () {
+	return `${this.firstName} ${this.lastName}`
+}
 
 // Instantiate object
 const person1 = new Person("John", "Doe", "4-3-1980");
 const person2 = new Person("Mary", "Smith", "3-6-1970");
 
-console.log(person2.dob.getFullYear())
+console.log(person1)
